fe1171443a0b3ae5b1e564ab51799f20cfc6f0f7
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-06-23 18:52:46 -0500

    some handlebars routes, not functioning yet

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,14 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const User = require('../models');
 
+router.get('/', (req, res) => {
+    res.render('homepage')
+});
+
+router.get('/chat', (req, res) => {
+    res.render('chat')
+});
+
 router.get('/login', (req, res) => {
     res.render('login');
 });
