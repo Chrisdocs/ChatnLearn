78fe717a9f89ed93de134700c048ba7a6dec3444
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-06-23 18:18:02 -0500

    user routes, update, delete, single

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -14,7 +14,31 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     })
 })
+
 // Get one User /api/users/id (get)
+router.get('/:id', (req, res) => {
+    User.findAll({
+        attributes: {
+            exclude: ['password']
+        },
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(userDbData => {
+        if (!userDbData) {
+            res.status(404).json({ message: 'No user found with this id.'})
+            return;
+        }
+        res.json(userDbData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+
 // Create a User /api/users (post)
 router.post('/', (req, res) => {
     User.create({
@@ -42,6 +66,43 @@ router.post('/', (req, res) => {
 // User Login (post)
 // User logout (post)
 // Update User (put)
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(userDbData => {
+        if (!userDbData) {
+            res.status(404).json({ message: 'No user found with this id. '})
+            return;
+        }
+        res.json(userDbData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
 // Delete User (delete)
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(userDbData => {
+        if (!userDbData) {
+            res.status(404).json({ message: 'No user found with this id.' })
+            return;
+        }
+        res.json(userDbData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
 
 module.exports = router
