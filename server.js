334aaff2b49a6433fdb07f97a88254bbf2c4254c
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-06-23 18:45:59 -0500

    server handlebars setup

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
+const helpers = require('./utils/helpers');
+const path = require('path');
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
 
 const PORT = process.env.PORT || 3001;
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.DB_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const {
     Server
 } = require("socket.io");
 
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view-engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(routes);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/html/index.html');
