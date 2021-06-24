848e29cd26f83069695d64b638273b1c0494d94b
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-06-23 21:23:50 -0500

    user can create account, cannot log in

---
+++ public/javascript/login.js
@@ -0,0 +1,58 @@
+async function signUpFormHandler(event) {
+    event.preventDefault();
+
+    const user_login = document.querySelector('#user-login-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const display_name = document.querySelector('#display-name-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (user_login && email && display_name && password) {
+        const res = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                user_login,
+                email,
+                display_name,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (res.ok) {
+            console.log('success');
+        } else {
+            alert(res.statusText);
+        }
+    }
+};
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const user_login = document.querySelector('#user-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (user_login && password) {
+        const res = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                user_login,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (res.ok) {
+            document.location.replace('/');
+        } else {
+            alert(res.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signUpFormHandler);
