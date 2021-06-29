4b36f26094d854c4c1d117f1b72ae7ec6faf0d29
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-06-28 19:17:32 -0500

    chat route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,6 +11,10 @@ router.get('/chat', (req, res) => {
     res.render('chat')
 });
 
+router.get('/user/:id/chat', (req, res) => {
+    res.render('chat')
+})
+
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
