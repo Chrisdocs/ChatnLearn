554533f9bec886feb1beb0978650d545783661d1
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-07-01 15:18:00 -0500

    brnach init

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const io = require('socket.io')(http);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const hbs = exphbs.create({ helpers });
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
 
@@ -38,6 +39,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('views/images'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(routes);
 
