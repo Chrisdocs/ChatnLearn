12edce3b0af986ecf7d45a1d86e016b426ce60d3
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-07-07 18:11:29 -0500

    database fix

--- models/friends.js
+++ models/friends.js
@@ -1,30 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-const User = require('./User')
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
+// const User = require('./User')
 
-class Friends extends Model {};
+// class Friends extends Model {};
 
-Friends.init({
-    friend_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        allowNull: false,
-        autoIncrement: true
-    },
-    user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: User,
-            key: 'id'
-        }
-    }
-}, {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'friends'
-});
+// Friends.init({
+//     friend_id: {
+//         type: DataTypes.INTEGER,
+//         primaryKey: true,
+//         allowNull: false,
+//         autoIncrement: true
+//     },
+//     user_id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         references: {
+//             model: User,
+//             key: 'id'
+//         }
+//     }
+// }, {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'friends'
+// });
 
-module.exports = Friends;
+// module.exports = Friends;
